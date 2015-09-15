5ec1c9d373b9f31b41eb7fc5053d73bbf083846b
朱伟亮 <zhuwl120820@gxwsxx.com>
2015-09-15 15:51:55 +0800

    调整优化异常的getMessage方法

--- tool-common/src/main/java/com/gxws/tool/common/exception/BaseException.java
+++ tool-common/src/main/java/com/gxws/tool/common/exception/BaseException.java
@@ -1,11 +1,14 @@
 package com.gxws.tool.common.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 /**
  * 基础定义异常，用于替代BaseException<br>
  * 重写{@link Exception}的getMessage()方法。<br>
  * 将BaseException对象和其子类对象定义为spring bean。<br>
- * 使用{@link BaseExceptionFactory}
- * 的get()方法获取具体的BaseException类或其子类对象。<br>
+ * 使用{@link BaseExceptionFactory} 的get()方法获取具体的BaseException类或其子类对象。<br>
  * 
  * @see java.lang.Exception
  * 
@@ -21,7 +24,9 @@ public class BaseException extends Exception {
 
 	private static final String SEP = ":";
 
-	protected StringBuilder message;
+	private StringBuilder message = new StringBuilder();
+
+	private Map<String, String> messageMap = new HashMap<>();
 
 	private String name = "undefind";
 
@@ -29,20 +34,26 @@ public class BaseException extends Exception {
 		this.message.append(PRE + message + SUF);
 	}
 
-	protected void appendMessageKey(String key) {
-		this.message.append(PRE + message);
-	}
-
-	protected void appendMessageValue(String value) {
-		this.message.append(SEP + message + SUF);
-	}
+	// protected void appendMessageKey(String key) {
+	// this.message.append(PRE + message);
+	// }
+	//
+	// protected void appendMessageValue(String value) {
+	// this.message.append(SEP + message + SUF);
+	// }
 
 	protected void appendMessage(String key, String value) {
-		this.message.append(PRE + key + SEP + value + SUF);
+		// this.message.append(PRE + key + SEP + value + SUF);
+		this.messageMap.put(key, value);
 	}
 
 	public String getMessage() {
-		return PRE + "异常 " + SEP + this.name + SUF + this.message.toString();
+		StringBuilder sb = new StringBuilder(PRE + "异常 " + SEP + this.name + SUF);
+		sb.append(message);
+		for (Entry<String, String> en : messageMap.entrySet()) {
+			sb.append(PRE + en.getKey() + SEP + en.getValue() + SUF);
+		}
+		return sb.toString();
 	}
 
 	public String toString() {
