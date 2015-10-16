27928922d8f564b24a0b9b8113ab86546ae24f9b
朱伟亮 <zhuwl120820@gxwsxx.com>
2015-10-16 21:17:45 +0800

    删除ExceptionFactory的调用方式，改为autowire方式注入。

--- tool-common/src/main/java/com/gxws/tool/common/exception/BaseException.java
+++ tool-common/src/main/java/com/gxws/tool/common/exception/BaseException.java
@@ -8,7 +8,6 @@ import java.util.Map.Entry;
  * 基础定义异常，用于替代BaseException<br>
  * 重写{@link Exception}的getMessage()方法。<br>
  * 将BaseException对象和其子类对象定义为spring bean。<br>
- * 使用{@link BaseExceptionFactory} 的get()方法获取具体的BaseException类或其子类对象。<br>
  * 
  * @see java.lang.Exception
  * 
@@ -33,14 +32,6 @@ public class BaseException extends Exception {
 	public void appendMessage(String message) {
 		this.message.append(PRE + message + SUF);
 	}
-
-	// protected void appendMessageKey(String key) {
-	// this.message.append(PRE + message);
-	// }
-	//
-	// protected void appendMessageValue(String value) {
-	// this.message.append(SEP + message + SUF);
-	// }
 
 	protected void appendMessage(String key, String value) {
 		// this.message.append(PRE + key + SEP + value + SUF);
